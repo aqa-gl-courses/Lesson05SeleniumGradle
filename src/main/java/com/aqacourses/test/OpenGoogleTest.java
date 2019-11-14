4335d99a6dbea3ffc09e0077c085ff6b1dd4a885
Alexey Voloshyn <vastland@gmail.com>
2019-11-14 11:42:26 +0200

    First test with selenium

---
+++ src/main/java/com/aqacourses/test/OpenGoogleTest.java
@@ -0,0 +1,67 @@
+package com.aqacourses.test;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import java.util.Collections;
+import java.util.concurrent.TimeUnit;
+
+public class OpenGoogleTest {
+
+    // Instance of WebDriver
+    private WebDriver driver;
+
+    /**
+     * Set up method
+     */
+    @Before
+    public void setUp() {
+
+        // If you want to disable infobars please use this code
+        ChromeOptions options = new ChromeOptions();
+        options.setExperimentalOption("excludeSwitches", Collections.singletonList("enable-automation"));
+        options.setExperimentalOption("useAutomationExtension", false);
+
+        // Initialize path to ChromeDriver
+        System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver.exe");
+
+        // Initialize instance of ChromeDriver and add options
+        driver = new ChromeDriver(options);
+
+        // Set 10 second for implicitly wait
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+
+        // Maximize window
+        driver.manage().window().maximize();
+    }
+
+    /**
+     * Open Google page, search and quit
+     */
+    @Test
+    public void testOpenGoogleTest() {
+
+        // Open Google
+        driver.get("https://google.com.ua");
+
+        // Send phrase in search input
+        driver.findElement(By.name("q")).sendKeys("AqaCources");
+    }
+
+    /**
+     * After method, quit driver
+     */
+    @After
+    public void tearDown() {
+
+        // Quit from Driver. close() just close window,
+        // quit() - close all window an driver
+        driver.quit();
+    }
+
+}
